b60ac3e5a9c7ec83b4fc70cef1867bd35fd6be1c
Jakob Schwendner <jakob.schwendner@dfki.de>
2010-12-16 12:16:16 +0100

    updated visualization interface and added some documentation

--- src/VizPlugin.cpp
+++ src/VizPlugin.cpp
@@ -1,14 +1,57 @@
-#include <osg/Node>
+#include <osg/Group>
 #include "VizPlugin.hpp"
 
 using namespace vizkit;
 
+/** this adapter is used to forward the update call to the plugin
+ */
+struct VizPluginBase::CallbackAdapter : public osg::NodeCallback 
+{
+    VizPluginBase* plugin;
+    CallbackAdapter( VizPluginBase* plugin ) : plugin( plugin ) {}
+    void operator()(osg::Node* node, osg::NodeVisitor* nv)
+    {
+	plugin->updateCallback( node );
+	osg::NodeCallback::operator()(node, nv);
+    }
+};
+
 VizPluginBase::VizPluginBase()
-    : dirty(false)
-    , ownNode(0) {}
+    : dirty( false )
+{
+    mainNode = new osg::Group();
+    nodeCallback = new CallbackAdapter( this );
 
+    mainNode->setUpdateCallback( nodeCallback );
+}
 
-void VizPluginBase::setMainNode(osg::Node* node) {
-    node->setUpdateCallback(this);
-    ownNode = node;
+osg::ref_ptr<osg::Group> VizPluginBase::getMainNode() const 
+{
+    return mainNode;
+}
+
+const std::string VizPluginBase::getPluginName() const 
+{
+    return "BaseDataNode";
+};
+
+void VizPluginBase::updateCallback(osg::Node* node)
+{
+    boost::mutex::scoped_lock lockit(updateMutex);
+
+    if( isDirty() )
+    {
+	updateMainNode(node->asGroup());
+	dirty = false;
+    }
+}
+
+bool VizPluginBase::isDirty() const
+{
+    return dirty;
+}
+
+void VizPluginBase::setDirty() 
+{
+    dirty = true;
 }
--- src/VizPlugin.hpp
+++ src/VizPlugin.hpp
@@ -1,77 +1,130 @@
-#ifndef DATANODE_H
-#define DATANODE_H
+#ifndef __VIZKIT_VIZPLUGIN_HPP__ 
+#define __VIZKIT_VIZPLUGIN_HPP__ 
 
 #include <osg/NodeCallback>
-#include <base/time.h>
-#include <boost/thread/mutex.hpp>
-#include <Eigen/Core>
-#include <yaml-cpp/yaml.h>
 #include <osg/Node>
+
+#include <boost/thread/mutex.hpp>
+#include <yaml-cpp/yaml.h>
 
 namespace vizkit 
 {
 
+/** 
+ * Interface class for all visualization plugins based on vizkit. All plugins
+ * provide an osg::Group() node, which can be added to an osg render tree for
+ * visualisation using getMainNode().
+ *
+ * The dirty handling works as such, that whenever the class is flagged dirty,
+ * the virtual updateMainNode() function will be called when it is safe to
+ * modify the node. Any plugin needs to implement this function to update the
+ * visualisation. The osg node must not be modified at any other time.
+ *
+ * The updateMainNode() is guarded by a mutex, so it is generally a good idea to
+ * guard any updates to the internal state of the plugin, that is required
+ * within the updateMainNode(). Note that updateMainNode() is most likely called
+ * from a different thread context than the rest.
+ */
 class VizPluginBase 
-    : public osg::NodeCallback
 {
     public:
         VizPluginBase();
 
-        virtual bool isDirty() const {
-            return dirty;
-        }
+	/** @return true if the plugins internal state has been updated */
+	virtual bool isDirty() const;
 
-	void setDirty() {
-	    dirty = true;
-	}
+	/** mark the internal state as modified */
+	void setDirty();
 
-	osg::Node *getNode() {
-	    return ownNode.get();
-	};
-        osg::Node *getMainNode() const {
-            return ownNode.get();
-        }
+	/** @return a pointer to the main node of the plugin */
+	osg::ref_ptr<osg::Group> getMainNode() const;
 
-        void setMainNode(osg::Node* node);	
+	/** @return the name of the plugin */
+	virtual const std::string getPluginName() const;
 
-	virtual void operator()(osg::Node* node, osg::NodeVisitor* nv) {
-	    {
-		boost::mutex::scoped_lock lockit(updateMutex);
-	    
-                if( isDirty() )
-                {
-                    operatorIntern(node, nv);
-                    dirty = false;
-                }
-	    }
-	    osg::NodeCallback::operator()(node, nv);
-	};
-	
-	virtual const std::string getPluginName() const {
-	  return "BaseDataNode";
-	};
+	/** overide this method to save configuration data. Always call the
+	 * superclass as well.
+	 * @param[out] emitter object which can be used to emit yaml structure
+	 *  containing configuration options
+	 */
 	virtual void saveData(YAML::Emitter& emitter) const {};
+
+	/** overide this method to load configuration data. Always call the
+	 * superclass as well.
+	 * @param[in] yamlNode object which contains previously saved
+	 *  configuration options
+	 */
 	virtual void loadData(const YAML::Node& yamlNode) {};
 
     protected:
+	/** override this function to update the visualisation.
+	 * @param node contains a point to the node which can be modified.
+	 */
+	virtual void updateMainNode(osg::Group* node) = 0;
+
+	/** lock this mutex outside updateMainNode if you update the internal
+	 * state of the visualization.
+	 */ 
 	boost::mutex updateMutex;
-        bool dirty;
-        osg::ref_ptr<osg::Node> ownNode;
-	virtual void operatorIntern(osg::Node* node, osg::NodeVisitor* nv) = 0;
+
+    private:
+	class CallbackAdapter;
+	osg::ref_ptr<osg::NodeCallback> nodeCallback;
+	void updateCallback(osg::Node* node);
+
+        osg::ref_ptr<osg::Group> mainNode;
+	bool dirty;
 };
 
+/** 
+ * convinience class template that performs the locking of incoming data.
+ * Derive from this class if you only have a single datatype to visualise, that
+ * can be easily copied.
+ */
 template <class T>
 class VizPlugin : public VizPluginBase
 {
     public:
+	/** updates the data to be visualised and marks the visualisation dirty
+	 * @param data const ref to data that is visualised
+	 */
 	void updateData(const T &data) {
 	    boost::mutex::scoped_lock lockit(this->updateMutex);
-            dirty = true;
+	    setDirty();
 	    updateDataIntern(data);
 	};
-	
+
     protected:
+	/** overide this method and set your internal state such that the next
+	 * call to updateMainNode will reflect that update.
+	 * @param data data to be updated
+	 */
 	virtual void updateDataIntern(const T &data) = 0;
+
+};
+
+/** @deprecated adapter item for legacy visualizations. Do not derive from this
+ * class for new designs. Use VizPlugin directly instead.
+ */
+template <class T>
+class VizPluginAdapter : public VizPlugin<T>
+{
+    protected:
+	virtual void operatorIntern( osg::Node* node, osg::NodeVisitor* nv ) = 0;
+
+	void updateMainNode( osg::Group* node )
+	{
+	    // NULL for nodevisitor is ok here, since its not used anywhere
+	    operatorIntern( node, NULL );
+	}
+
+	void setMainNode( osg::Node* node )
+	{
+	    this->getMainNode()->addChild( node );
+	}
+
+    protected:
+	osg::ref_ptr<osg::Node> ownNode;
 };
 
 }
