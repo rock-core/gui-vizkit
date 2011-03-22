45b6992fa13736357518d3cf22d8a97c99e50e88
Alexander Duda <Alexander.Duda@dfki.de>
2011-03-22 18:02:04 +0100

    improve task.port.connect_to do |data,_| if task is a Log::Task no qt timer is used

--- ruby/lib/vizkit/output_port.rb
+++ ruby/lib/vizkit/output_port.rb
@@ -10,15 +10,18 @@ module Orocos
         Vizkit.connections << Vizkit::OQLogConnection.new(self, options,widget,&block)
       end
 
+      #code blocks are called directly from Log::Replay if widet == nil
+      #all other connect_to calls are setting up a qt timer
+      #which calls reader.read with 20Hz if update_frequency is not given
       def connect_to(widget=nil, options = Hash.new,&block)
         if widget.is_a?(Hash)
           options = widget
           widget = nil
         end
-        if widget.is_a?(Qt::Widget)||block_given?|| widget.is_a?(Method)
+        if widget.is_a?(Qt::Widget)|| widget.is_a?(Method)
           return connect_to_widget(widget,options,&block)
         else
-          return org_connect_to widget,options
+          return org_connect_to widget,options,&block
         end
         self
       end
@@ -34,6 +37,10 @@ module Orocos
       Vizkit.connections << Vizkit::OQConnection.new(self, options,widget,&block)
     end
 
+    #sets up a qt timer
+    #which calls reader.read with 20Hz if update_frequency is not given
+    #TODO if only a code block is given we should use a call back 
+    #to get the same behavior like for Log::OutputPort
     def connect_to(widget=nil, options = Hash.new,&block)
       if widget.is_a?(Hash)
         options = widget
