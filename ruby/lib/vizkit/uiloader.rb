b8f100e71272585ef8331cb90d2e98fc3c4a1b4f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-04-11 14:21:53 +0200

    ruby: add support to use modules to create "subclasses" of C++ widgets in Ruby

--- ruby/lib/vizkit/uiloader.rb
+++ ruby/lib/vizkit/uiloader.rb
@@ -7,6 +7,45 @@ require 'delegate'
 require 'rexml/document'
 require 'rexml/xpath'
 
+class Module
+  # Shortcut to define the necessary methods so that a module can be used to
+  # "subclass" a Qt widget
+  #
+  # This is done with
+  #
+  #   require 'vizkit'
+  #   module MapView
+  #     vizkit_subclass_of 'ImageView'
+  #   end
+  #   Vizkit::UILoader.register_ruby_widget 'MapView', MapView.method(:new)
+  #
+  # If some initial configuration is needed, one should define the 'setup'
+  # singleton method:
+  #
+  #   module MapView
+  #     vizkit_subclass_of 'ImageView'
+  #     def self.setup(obj)
+  #       obj.setAspectRatio(true)
+  #     end
+  #   end
+  #
+  def vizkit_subclass_of(class_name)
+    class_eval do
+      def self.new
+        widget = Vizkit.default_loader.send(class_name)
+        widget.extend self
+        widget
+      end
+      def self.extended(obj)
+        if respond_to?(:setup)
+          setup(obj)
+        end
+      end
+    end
+  end
+end
+
+
 module Vizkit
   #because of the shadowed method load we have to use DelegateClass
   class UiLoader < DelegateClass(Qt::UiLoader)
