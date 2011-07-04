82acab47b6729cb085a853518fcec7ecf251da97
Alexander <aduda@aduda-l.localdomain>
2011-07-04 14:17:21 +0200

    vizkit.rb update task_inspector
    it supports now editing attributes (still not fully tested) and
    reconnect

--- ruby/lib/vizkit/widgets/task_inspector/task_inspector.rb
+++ ruby/lib/vizkit/widgets/task_inspector/task_inspector.rb
@@ -3,9 +3,11 @@
 class TaskInspector < Qt::Widget
   MAX_ARRAY_FIELDS = 32
 
-  slots 'refresh()','set_task_attribute(const QModelIndex&)'
+  slots 'refresh()','set_task_attribute(QStandardItem*)'
   attr_reader :multi  #widget supports displaying of multi tasks
   PropertyConfig = Struct.new(:name, :attribute, :type)
+  DataPair = Struct.new :name, :task
+
 
   def initialize(parent=nil)
     super
@@ -19,13 +21,14 @@ class TaskInspector < Qt::Widget
     @window.treeView.setAlternatingRowColors(true)
     @window.treeView.setSortingEnabled(true)
     @brush = Qt::Brush.new(Qt::Color.new(200,200,200))
-    @hash = Hash.new
-    @mapping = Hash.new
-    @tasks = Hash.new
-    @multi = true
 
-    # connect(@tree_model, SIGNAL('dataChanged(const QModelIndex&,const QModelIndex&)'),self,SLOT('set_task_attribute(const QModelIndex&)'))
-    
+    @hash = Hash.new
+    @reader_hash = Hash.new
+    @tasks = Hash.new
+
+    @multi = true
+    connect(@tree_model, SIGNAL('itemChanged(QStandardItem*)'),self,SLOT('set_task_attribute(QStandardItem*)'))
+
     @timer = Qt::Timer.new(self)
     connect(@timer,SIGNAL('timeout()'),self,SLOT('refresh()'))
   end
@@ -37,282 +40,214 @@ class TaskInspector < Qt::Widget
   end
 
   def config(task,options=Hash.new)
-    @tasks[task.name] = task if !@tasks.has_key?(task.name)
+    data_pair = DataPair.new
+    if task.is_a? Orocos::TaskContext
+      data_pair.name = task.name
+      data_pair.task = task
+    else
+      data_pair.name = task
+    end
+    @tasks[data_pair.name] = data_pair if !@tasks.has_key?(data_pair.name)
     options = default_options.merge(options)
     @timer.start(options[:interval])
   end
 
-  def get_item(key,name,root_item)
-    if @hash.has_key?(key)
-       item =  @hash[key]
-       item2 = root_item.child(item.row,1)
-    else
-      item = Qt::StandardItem.new(name)
-      item.setEditable(false)
-      @hash[key]=item
-      root_item.appendRow(item)
+  def child_items(parent_item,row)
+    item = parent_item.child(row,0)
+    item2 = parent_item.child(row,1)
+    unless item
+      item = Qt::StandardItem.new
+      parent_item.appendRow(item)
       item2 = Qt::StandardItem.new
+      parent_item.setChild(item.row,1,item2)
+      item.setEditable(false)
       item2.setEditable(false)
-      root_item.setChild(item.row,1,item2)
     end
-    return [item,item2]
+    [item,item2]
   end
 
-  def add_object(object, parent_item)
-    if object.is_a?(Orocos::Property)
-	object = object.read
+  def get_item(key,name,root_item)
+    if @hash.has_key?(key)
+      item =  @hash[key]
+      item2 = root_item.child(item.row,1)
+      [item,item2]
+    else
+      item, item2 = child_items root_item, -1
+      item.setText name.to_s
+      @hash[key]=item
+      [item,item2]
     end
-    if object.is_a?(Orocos::OutputReader)
-	object = object.read
-    end
+  end
 
+  def update_item(object, parent_item,read_obj=false,row=0,name_hint=nil)
     if object.kind_of?(Typelib::CompoundType)
       row = 0;
       object.each_field do |name,value|
-        item, item2 = StructViewer.child_items(parent_item,row)
-	item.setEditable(false)
-        item.setText name
-        item2.setText value.class.name
-        add_object(value,item)
-        # item.setBackground(@brush)
-        # item2.setBackground(@brush)
+        if read_obj
+          object.set_field(name,update_item(value,parent_item,read_obj,row,name))
+        else
+          update_item(value,parent_item,read_obj,row,name)
+        end
         row += 1
       end
       #delete all other rows
       parent_item.removeRows(row,parent_item.rowCount-row) if row < parent_item.rowCount
-
     elsif object.is_a?(Array) || (object.kind_of?(Typelib::Type) && object.respond_to?(:each) )
       if object.size > MAX_ARRAY_FIELDS
-        item2 = parent_item.parent.child(parent_item.row,parent_item.column+1)
+        item, item2 = child_items(parent_item,0)
         item2.setText "#{object.size} fields ..."
-      else
+      elsif object.size > 0
         row = 0
         object.each_with_index do |val,row|
-          item,item2 = StructViewer.child_items(parent_item,row)
-	  item.setEditable(false)
-          item2.setText val.class.name
-          item.setText "[#{row}]"
-          add_object val,item
+          if read_obj
+            object[row]=  update_item(val,parent_item,read_obj,row)
+          else
+            update_item(val,parent_item,read_obj,row)
+          end
+          row += 1
         end
         #delete all other rows
-        row += 1
         parent_item.removeRows(row,parent_item.rowCount-row) if row < parent_item.rowCount
+      elsif read_obj
+        a = (update_item(object.to_ruby,parent_item,read_obj,0))
+        object << a
       end
     else
-      item2 = parent_item.parent.child(parent_item.row,parent_item.column+1)
-      item2.setText(object.to_s)
+      item, item2 = child_items(parent_item,row)
+      if object 
+        if read_obj
+          raise "name differs" if(object.respond_to?(:name) && item.text != object.name)
+          #confert type
+          type = object
+          if object.is_a? Typelib::Type
+            type = object.to_ruby 
+          end
+          data = item2.text if type.is_a? String
+          data = item2.text.to_f if type.is_a? Float
+          data = item2.text.to_i if type.is_a? File
+          data = item2.text.to_i == 0 if type.is_a? FalseClass
+          data = item2.text.to_i == 1 if type.is_a? TrueClass
+          data = item2.text.to_sym if type.is_a? Symbol
+          data = Time.new(item2.text) if type.is_a? Time
+          if object.is_a? Typelib::Type
+            Typelib.copy(object,Typelib.from_ruby(data, object.class))
+          else
+            object = data
+          end
+        else
+          if object.respond_to? :name
+            item.setText object.name 
+          elsif name_hint
+            item.setText name_hint
+          else
+            item.setText "[#{row}]"
+          end
+          item2.setText object.to_s 
+        end
+      else
+        raise 'No data available' if read_obj
+        item2.setText "no samples received" 
       end
+    end
+    object
+  end
+
+  def port_reader(task, port)
+    readers = @reader_hash[task]
+    if !readers
+      readers = Hash.new 
+      @reader_hash[task] = readers
+    end
+    #we have to use the name of the port
+    #because task.port returns a new object
+    #each time it is called
+    reader = readers[port.name]
+    if !reader
+      reader = port.reader :pull => true
+      readers[port.name] = reader
+    end
+    reader
+  end
+
+  def delete_port_reader(task)
+    @reader_hash[task] = nil
   end
 
 
   def refresh()
-    @tasks.each_value do |task|
-      item, item2 = get_item(task.name,task.name, @root_item)
-      item2.setText(task.state.to_s) 
-      #setting attributes
-      key = task.name + "__ATTRIBUTES__"
-      item3, item4 = get_item(key,"Attributes", item)
-      
-      #setChild(item.row,1,item2)
-      task.each_property do |attribute|
-      name = attribute.name
-      if !@hash.has_key?(name)
-	      itemm = Qt::StandardItem.new(name)
-	      itemm.setEditable(false)
-	      itemm2 = Qt::StandardItem.new
-	      #itemm2.setText(attribute.class.to_s.match('/(.*)>$')[1])
-	      itemm2.setText(attribute.name)
-	      itemm2.setEditable(true)
-	      @hash[name]=itemm
-	      @mapping[itemm] = PropertyConfig.new(name,attribute,attribute.read.class)
-	      item3.appendRow(itemm)
-	      item3.setChild(itemm.row,1,itemm2)
-	      add_object(attribute,itemm)
-      end
-      end
-#        key = task.name+"_"+ attribute.name
-#        item5, item6 = get_item(key,attribute.name, item3)
-#        item6.setText(attribute.read.to_s)
-#        if attribute.read.is_a?(String)||attribute.read.is_a?(Float)||attribute.read.is_a?(Fixnum)
-#          if !@mapping.has_key?(item6)
-#            @mapping[item6] = PropertyConfig.new(key,attribute,attribute.read.class)
-#            item6.setEditable(true)
-#          end
-#        end
-
-      #setting ports
-      key = task.name + "__IPORTS__"
-      key2 = task.name + "__OPORTS__"
-      item3, item4 = get_item(key,"Input Ports", item)
-      item5, item6 = get_item(key2,"Output Ports", item)
-
-      task.each_port do |port|
-        key = task.name+"_"+ port.name
-        if port.is_a?(Orocos::InputPort)
-          item7, item8 = get_item(key,port.name, item3)
-        else
-	  name = port.name.to_s
-          if !@hash.has_key?(name)
-	    itemm = Qt::StandardItem.new(name)
-            itemm2 = Qt::StandardItem.new
-            #itemm2.setText(attribute.class.to_s.match('/(.*)>$')[1])
-            itemm2.setText(name)
-            reader = port.reader :pull => true, :init => true
-            @hash[name]=[itemm, reader]
-            item5.appendRow(itemm)
-            item5.setChild(itemm.row,1,itemm2)
-            add_object(port,itemm)
-            #item7, item8 = get_item(key,port.name, item5)
-          else
-            itemm, reader = @hash[name]
-            add_object(reader,itemm)
-          end
+    @tasks.each_value do |pair|
+      #check if task is still available and try 
+      #to reconnect if not
+      if !pair.task || !pair.task.reachable?
+        delete_port_reader pair.task if pair.task
+        begin
+          pair.task = Orocos::TaskContext.get pair.name
+        rescue Orocos::NotFound 
+          pair.task = nil
         end
-        #item8.setText(port.type_name.to_s)
+      end
+
+      item, item2 = get_item(pair.name,pair.name, @root_item)
+      if !pair.task
+        item2.setText("not reachable")
+        item.removeRows(1,item.rowCount-1)
+      else
+        begin
+          task = pair.task
+          item2.setText(task.state.to_s) 
+          #setting attributes
+          key = task.name + "__ATTRIBUTES__"
+          item3, item4 = get_item(key,"Attributes", item)
+
+          task.each_property do |attribute|
+            key = task.name+"_"+ attribute.name
+            item5, item6 = get_item(key,attribute.name, item3)
+            if attribute.read.is_a?(String)||attribute.read.is_a?(Float)||attribute.read.is_a?(Fixnum)
+              item6.setEditable true
+              item6.setText(attribute.read.to_s) 
+              @hash[item6]=pair if !@hash.has_key? item6
+            else
+              update_item(attribute.read,item5)
+            end
+          end
+
+          #setting ports
+          key = task.name + "__IPORTS__"
+          key2 = task.name + "__OPORTS__"
+          item3, item4 = get_item(key,"Input Ports", item)
+          item5, item6 = get_item(key2,"Output Ports", item)
+
+          task.each_port do |port|
+            key = task.name+"_"+ port.name
+            if port.is_a?(Orocos::InputPort)
+              item7, item8 = get_item(key,port.name, item3)
+            else
+              item7, item8 = get_item(key,port.name, item5)
+              reader = port_reader(task,port)
+              update_item(reader.read,item7)
+            end
+            item8.setText(port.type_name.to_s)
+          end
+        rescue Orocos::CORBA::ComError
+          pair.task = nil
+        end
       end
     end
-
     @window.treeView.resizeColumnToContents(0)
   end
-  
-  def set_task_attribute(pos)
-    #pp "somethig changed at pos #{pos}"
-    item = @tree_model.itemFromIndex(pos)
-    value = item.text
-    #pp @mapping
-    #pp "somethig changed at pos #{pos} on item #{item}"
-    #pp item.name
-    #pp pos
-    
-    obj = nil
-    original_item = item
-    #pp "Position #{pos.row} #{pos.column}"
-    parent = item.parent
-    left_object = nil
-    if !parent.nil?
-      left_object = parent.child(item.row,0)
-    end
-    name = ''
-    first=true
-    if !left_object.nil? 
-      #name = left_object.text
-      item = left_object
-      first=false
-    end
 
-    while obj.nil? && !item.nil? do
-	if @mapping.has_key?(item)
-	  obj = @mapping[item]
-        else 
-          if !first
-            name = "#{item.text}.#{name}"
-            first=false
-          else
-	    name = item.text
-	  end
-	  item = item.parent
-        end
-    end
-    if name.to_s.end_with?('.')
-      name = name.to_s.chop
-    end
-    
-      
-    if name == "StructViewer"
-    	return
-    end
+  def set_task_attribute(item2)
+    return if !item2.parent
+    item = item2.parent.child(item2.row,0) 
+    pair = @hash[item2]
+    return if !pair || !pair.task || !pair.task.reachable?
 
-    if obj.nil?
-#      STDERR.puts ""
-#      STDERR.puts "Cannot find correct object for mapping:"
-#      pp @mapping
-#      STDERR.puts "------------original item is:-------"
-#      pp original_item
-#      STDERR.puts "------------------end--------------"
-
-      #pp "We should set #{name} to #{value}"
-      return 
-    end
-    #pp "We have Object: "
-    #pp "We should set #{name} to #{value} "
-
-    #name.to_sym
-    data = obj.attribute.read
-    #pp "Weve got data: #{data}"
-
-    begin
-    #  pp "hall"
-
-    type = nil
-    if(name.empty?)
-      type = data.class
-    else
-      f = data
-      name.split(".").each do |s|
-        f = f.send(s)
-      end
-      type = f.class
-    end
-
-
-    if type == String
-      if name.empty?
-      	data = value
-      else
-      	eval "data.#{name} = value"
-      end
-      obj.attribute.write(data)
-    elsif type == Fixnum
-      if name.empty?
-        data = value.to_i
-      else
-      	eval "data.#{name} = value.to_i"
-      end
-      pp "writing fixednum"
-      obj.attribute.write(data)
-    elsif type == Float
-      if name.empty?
-        data = value.to_i
-      else
-        eval "data.#{name} = value.to_i"
-      end
-      obj.attribute.write(data)
-    elsif type == TrueClass || type == FalseClass 
-      if value == "true" || value == "1"
-        if name.empty?
-	  data = true
-	else
-          eval "data.#{name} = true"
-	end
-	obj.attribute.write(data)
-      elsif value == "false" || value == "0"
-        if name.empty?
-	  data = false
-	else
-          eval "data.#{name} = false"
-	end
-	obj.attribute.write(data)
-      else
-        STDERR.puts "Unknown Value #{value} for Boolean skipping"
-      end
-    else
-      #pp "----name ----"
-      #pp name
-      #pp "---object----"
-      #pp obj
-      #pp "---obj.attribue---"
-      #pp obj.attribute
-      #pp "---end---"
-      puts "task_inspector::set_task_attribute() not implemented for " + type.name
-      #pp data.send(name)
-    end
-
-    rescue Exception => e
-      STDERR.puts "Error #{e}"
-      #pp e.backtrace
-    end
+    property = pair.task.property item.text
+    sample = property.new_sample
+    update_item(sample,item.parent,true,item.row)
+    property.write sample
   end
-  
 end
+
 
 Vizkit::UiLoader.register_ruby_widget("task_inspector",TaskInspector.method(:new))
