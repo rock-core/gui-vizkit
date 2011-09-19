8d1f8586c5d3d44215cb526cd91adf294616c424
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-09-19 16:58:57 +0200

    ruby: define registration methods for 3D plugins
    The new methods allow to register vizkit3D plugins into the automatic
    type-to-widget association infrastructure. This, among other things,
    allows to use Vizkit3D displays with rock-replay
    
    For now, all the plugins are displayed on the same 3D widget

--- ruby/lib/vizkit/uiloader.rb
+++ ruby/lib/vizkit/uiloader.rb
@@ -64,6 +64,30 @@ module Vizkit
       def extend_cplusplus_widget_class(class_name,&block)
         @current_loader_instance.extend_cplusplus_widget_class(class_name,&block)
       end
+      def register_3d_plugin(widget_name,lib_name,plugin_name,widget_method_name,plugin_method_name=:updateData)
+        # This is used to share the plugin instance between the creation method
+        # and the display method
+        plugin = nil
+        ext = Module.new do
+          define_method(widget_method_name) do |data,port|
+            plugin.send(plugin_method_name,data)
+          end
+        end
+        creation_method = lambda do |parent|
+          if !Orocos.initialized?
+            Orocos.initialize
+          end
+          widget = Vizkit.vizkit3d_widget
+          widget.extend(ext)
+          plugin = widget.createPlugin(lib_name, plugin_name)
+          widget
+        end
+        register_ruby_widget(widget_name,creation_method)
+      end
+      def register_3d_plugin_for(class_name,type_name,update_method)
+        @current_loader_instance.register_callback_fct('vizkit::Vizkit3DWidget',type_name,update_method)
+        register_widget_for(class_name,type_name,update_method)
+      end
 
       def define_widget_for_methods(name,*klasses,&map)
         if klasses.last != :no_auto     #widget can not be reached via widget_for value if no_auto is set
@@ -362,14 +386,18 @@ module Vizkit
       self
     end
 
+    def register_callback_fct(class_name,value,callback_fct=:update)
+      @callback_fct_hash[class_name] ||= Hash.new
+      @callback_fct_hash[class_name][value] = callback_fct
+    end
+
     def register_widget_for(class_name,value,callback_fct=:update)
       #check if widget is available
       if !widget? class_name
  #       puts "Widget #{class_name} is unknown to the loader. Cannot extend it!" 
         return nil
       end
-      @callback_fct_hash[class_name] ||= Hash.new
-      @callback_fct_hash[class_name][value] = callback_fct
+      register_callback_fct(class_name,value,callback_fct)
 
       @widget_for_hash[value] ||= Array.new
       @widget_for_hash[value] << class_name if !@widget_for_hash[value].include?(class_name)
