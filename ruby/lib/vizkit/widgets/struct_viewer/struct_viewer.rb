089908469db270139eb1d927da9b36eac610cc3b
Allan E. Conquest <allan.conquest@dfki.de>
2011-09-28 14:02:34 +0200

    Removed example and debug outputs.

--- ruby/lib/vizkit/widgets/struct_viewer/struct_viewer.rb
+++ ruby/lib/vizkit/widgets/struct_viewer/struct_viewer.rb
@@ -15,21 +15,14 @@ class StructViewer < Qt::Widget
     @window.treeView.set_alternating_row_colors(true)
     @window.treeView.set_sorting_enabled(true)
     
-    #@root_item = @tree_model.invisible_root_item
-    #@cool_item = Qt::StandardItem.new("cool.item")
-    #@root_item.append_row(@cool_item)
-    
   end
 
   def update(data, port_name)
-     #@modeler.generate_tree(data, port_name, @tree_model)
-     puts "*** Updating port_name: #{port_name}"
+     #puts "*** Updating port_name: #{port_name}"
      
      @modeler.generate_sub_tree(data, port_name, @tree_model.invisible_root_item)
-
-     #@modeler.generate_sub_tree(data, port_name, @cool_item)
      
-     puts "*** root item rowCount = #{@tree_model.invisible_root_item.row_count}"
+     #puts "*** root item rowCount = #{@tree_model.invisible_root_item.row_count}"
   end
 
 end
