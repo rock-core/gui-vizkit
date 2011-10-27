0c055d7e0d6cade626bee6c1593e4b91d0df70da
Alexander Duda <Alexander.Duda@dfki.de>
2011-10-27 11:09:04 +0200

    vizkit.rb bugfix related to sample drops
    vizkit was always using a qt timer for reading samples from log files.
    Now, all code blocks and method are called for each sample

--- ruby/lib/vizkit/output_port.rb
+++ ruby/lib/vizkit/output_port.rb
@@ -13,18 +13,21 @@ module Orocos
         connection 
       end
 
-      #code blocks are called directly from Log::Replay if widet == nil
-      #all other connect_to calls are setting up a qt timer
-      #which calls reader.read with 20Hz if update_frequency is not given
+      #code blocks and methods are called directly from Log::Replay if now widget is given
       def connect_to(widget=nil, options = Hash.new,&block)
         if widget.is_a?(Hash)
           options = widget
           widget = nil
         end
-        if !widget || widget.is_a?(Qt::Widget)|| widget.is_a?(Method)
+        if(widget && widget.is_a?(Qt::Widget))
           return connect_to_widget(widget,options,&block)
         else
-          return org_connect_to widget,options,&block
+          if(widget && widget.is_a?(Method))
+            raise "Cannot connect to a method and a code block at the same time. Call connect_to twice." if block_given?
+            return org_connect_to nil,options,&widget.to_proc
+          else
+            return org_connect_to widget,options,&block
+          end
         end
         self
       end
