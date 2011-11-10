df17496737cb32a5b9aa769c33f33940d977b472
Alexander Duda <alexander.duda@dfki.de>
2011-11-10 23:21:03 +0100

    vizkit.rb: improve TaskProxy
    add code block which is called during connect/reconnect

--- ruby/lib/vizkit/task_proxy.rb
+++ ruby/lib/vizkit/task_proxy.rb
@@ -3,34 +3,48 @@
 #this is useful if the task context is needed before 
 #the task was started or if the task was restarted
 module Vizkit
-  class TaskProxy
-    def initialize(task_name)
-      @__task_name = task_name
-      @__task = Vizkit.use_task? task_name
+    class TaskProxy
+        def initialize(task_name,&block)
+            @__task_name = task_name
+            @__task = Vizkit.use_task? task_name
+            @__connection_code_block = block
+        end
+
+        #code block is called every time a new connection is set up
+        def __on_connect(&block)
+            @__connection_code_block = block
+        end
+
+        def __reconnect()
+            @task = nil
+            ping
+        end
+
+        def ping
+            if !@__task || !@__task.reachable?
+                begin 
+                    @__task = Orocos::TaskContext.get(@__task_name)
+                    @__connection_code_block.call if @__connection_code_block
+                rescue Orocos::NotFound
+                    @__task = nil
+                end
+            end
+            @__task != nil
+        end
+
+        def method_missing(m, *args, &block)
+            if !ping
+                return
+            end
+
+            begin
+                @__task.send(m, *args, &block)
+            rescue Orocos::NotFound
+                @__task = nil
+            end
+        end
+
+        alias :reachable? :ping
+        alias :__connect :ping
     end
-
-    def ping
-      if !@__task || !@__task.reachable?
-        @__task =
-          begin Orocos::TaskContext.get(@__task_name)
-          rescue Orocos::NotFound
-            @__task = nil
-          end
-      end
-      @__task != nil
-    end
-
-    def method_missing(m, *args, &block)
-      if !ping
-        return
-      end
-
-      begin
-      @__task.send(m, *args, &block)
-      rescue Orocos::NotFound
-      end
-    end
-
-    alias :reachable? :ping
-  end
 end
