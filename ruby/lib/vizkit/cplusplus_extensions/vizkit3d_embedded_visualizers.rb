7c8f36a0f00aac3618b993b07855c55644e28348
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-11-23 17:12:05 +0100

    declare the vizkit3d embedded RBS viewer so that it can be used in display and replay

---
+++ ruby/lib/vizkit/cplusplus_extensions/vizkit3d_embedded_visualizers.rb
@@ -0,0 +1,2 @@
+Vizkit::UiLoader.register_3d_plugin('RigidBodyStateVisualization', 'RigidBodyStateVisualization', nil)
+Vizkit::UiLoader.register_3d_plugin_for('RigidBodyStateVisualization', "/base/samples/RigidBodyState_m", :updateRigidBodyState)
