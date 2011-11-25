f277e879f6656d8ff5ef413a6081614ec2b20331
Alexander Duda <Alexander.Duda@dfki.de>
2011-11-25 16:47:23 +0100

    vizkit.rb add proxy reader

--- ruby/lib/vizkit/task_proxy.rb
+++ ruby/lib/vizkit/task_proxy.rb
@@ -8,6 +8,7 @@ module Vizkit
             @__task_name = task_name
             @__task = Vizkit.use_task? task_name
             @__connection_code_block = block
+            @__readers = Hash.new
         end
 
         #code block is called every time a new connection is set up
@@ -23,6 +24,7 @@ module Vizkit
         def ping
             if !@__task || !@__task.reachable?
                 begin 
+                    @__readers.clear
                     @__task = Orocos::TaskContext.get(@__task_name)
                     @__connection_code_block.call if @__connection_code_block
                 rescue Orocos::NotFound
@@ -30,6 +32,13 @@ module Vizkit
                 end
             end
             @__task != nil
+        end
+
+        def __reader_for_port(port_name,options=Hash.new)
+            if ping && !@__readers[port_name]
+                @__readers[port_name] = port(port_name).reader options
+            end
+            @__readers[port_name]
         end
 
         def method_missing(m, *args, &block)
