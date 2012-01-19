ca2a01bf879d49b0c79028a0a1120f91b7f263bf
Alexander Duda <Alexander.Duda@dfki.de>
2012-01-19 10:34:03 +0100

    vizkit.rb: SonarDisplay is automatically reconfigured
    --> settings are automatically change to match the configuration
        of the sonar beams

--- ruby/lib/vizkit/cplusplus_extensions/sonar_display.rb
+++ ruby/lib/vizkit/cplusplus_extensions/sonar_display.rb
@@ -5,7 +5,21 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "SonarDisplay" do
   end
 
   def display(sonar_beam,port_name)
-     addSonarBeam(sonar_beam.bearing.rad+Math::PI,sonar_beam.beam.size-8,sonar_beam.beam.to_byte_array[8..-1])
+      @resolution ||= 0.1
+      @number_of_bins ||= 100
+
+      angle = sonar_beam.bearing.rad + Math::PI*0.5 
+      angle +=  Math::PI*2 if angle < 0
+      data = sonar_beam.beam.to_byte_array[8..-1]
+      resolution = sonar_beam.sampling_interval*sonar_beam.speed_of_sound*0.5
+
+      if(@resolution != resolution || @number_of_bins < data.size)
+          setUpSonar(72,data.size, 5.0/180*Math::PI,resolution,sonar_beam.beamwidth_vertical)
+          @number_of_bins = data.size
+          @resolution = resolution
+      end
+
+      addSonarBeam(angle,data.size,data)
   end
 end
 
