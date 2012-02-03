059f213b5e16a7401bab11069aed3e9b3fe8c0d1
Alexander Duda <Alexander.Duda@dfki.de>
2012-02-03 10:24:59 +0100

    add readerproxy

--- ruby/lib/vizkit/task_proxy.rb
+++ ruby/lib/vizkit/task_proxy.rb
@@ -3,6 +3,72 @@
 #this is useful if the task context is needed before 
 #the task was started or if the task was restarted
 module Vizkit
+
+    #the reader proxy is meant for graphical interfaces
+    #if an update_frequency is given the reader will start a new 
+    #thread to read from the port to not block the main thread
+    class ReaderProxy
+        def initialize(task_proxy,port_name,buffer_size=1,update_frequency=0)
+            @task_proxy = task_proxy
+            if(@task_proxy.is_a? String)
+                @task_proxy = TaskProxy.new(task_proxy)
+            end
+
+            @__reader = nil
+            @port_name = port_name
+            @buffer_size = buffer_size
+            @update_frequency = update_frequency
+
+            @buffer = Array.new
+            @thread = Thread.new{}
+
+            @timer = Qt::Timer.new
+            @timer.connect(SIGNAL('timeout()')) do 
+                if @thread.alive?
+                    if @thread.key? :sample
+                        @buffer.shift if @buffer.size >= @buffer_size
+                        @buffer.push @thread[:sample]
+                    end
+                    @thread = Thread.new(__reader()) do |reader|
+                        begin
+                            Thread.current[:sample] = reader.read if reader
+                        rescue Orocos::NotFound
+                            self
+                        end
+                    end
+                end
+            end
+            @timer.start(1/@update_frequency) if update_frequency > 0
+        end
+
+        def __reader
+            if @task_proxy.ping && !@__reader
+                @__reader = @task_proxy.port(port_name).reader :pull => true, :type => :buffer,:size => @buffer_size
+            end
+            @__reader
+        end
+
+        def read
+            if @update_frequency == 0
+                if __reader
+                    begin
+                        __reader.read
+                    rescue Orocos::NotFound
+                        nil
+                    end
+                else
+                    nil
+                end
+            else
+                if !@buffer.empty?
+                    @buffer.shift
+                else
+                    nil
+                end
+            end
+        end
+    end
+
     class TaskProxy
         attr_accessor :__task
 
