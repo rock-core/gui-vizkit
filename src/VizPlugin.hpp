2e73c66a6d07be1838d1a243d21b8da29c533955
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-02-06 16:23:10 +0100

    cleanup inclusions, installed headers and CMake code

--- src/VizPlugin.hpp
+++ src/VizPlugin.hpp
@@ -4,6 +4,8 @@
 
 //this header is only here for backward compatibillity
 
+#warning "vizkit/VizPlugin.hpp and vizkit::VizPlugin<T> are deprecated. Use <vizkit/Vizkit3DPlugin> and vizkit::Vizkit3DPlugin<T>, which are drop-in replacements"
+
 namespace vizkit {
 
 template <class T>
