9613c71eb4bea3ca93bd8fc32f52c8b82c2d76f8
Alexander Duda <alexander.duda@dfki.de>
2012-02-28 08:25:00 +0100

    vizkit.rb plot2d: add support for plotting SonarBeam

--- ruby/lib/vizkit/cplusplus_extensions/plot2d.rb
+++ ruby/lib/vizkit/cplusplus_extensions/plot2d.rb
@@ -11,6 +11,7 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "Plot2d" do
         options[:colors] = [Qt::red, Qt::green, Qt::blue, Qt::cyan, Qt::magenta, Qt::yellow, Qt::gray]
         options[:reuse] = true
         options[:use_y_axis2] = false
+        options[:multi_use_menu] = true
         return options 
     end
 
@@ -29,30 +30,32 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "Plot2d" do
             @options = default_options
             @graphs = Hash.new
             @time = time.to_f
-            
+
             getLegend.setVisible(true)
             getXAxis.setLabel("Time in sec")
             setTitle("Rock-Plot2d")
             self.connect(SIGNAL('mousePress(QMouseEvent*)')) do |event|
-                if event.button() == Qt::RightButton
+                if event.button() == Qt::RightButton 
                     #show pop up menue 
                     menu = Qt::Menu.new(self)
                     action_scrolling = Qt::Action.new("AutoScrolling", self)
                     action_scrolling.checkable = true
                     action_scrolling.checked = @options[:auto_scrolling]
                     menu.add_action(action_scrolling)
-                    action_reuse = Qt::Action.new("Reuse Widget", self)
-                    action_reuse.checkable = true
-                    action_reuse.checked = @options[:reuse]
-                    menu.add_action(action_reuse)
-                    action_use_y2 = Qt::Action.new("Use 2. Y-Axis", self)
-                    action_use_y2.checkable = true
-                    action_use_y2.checked = @options[:use_y_axis2]
-                    menu.add_action(action_use_y2)
+                    if @options[:multi_use_menu]
+                        action_reuse = Qt::Action.new("Reuse Widget", self)
+                        action_reuse.checkable = true
+                        action_reuse.checked = @options[:reuse]
+                        menu.add_action(action_reuse)
+                        action_use_y2 = Qt::Action.new("Use 2. Y-Axis", self)
+                        action_use_y2.checkable = true
+                        action_use_y2.checked = @options[:use_y_axis2]
+                        menu.add_action(action_use_y2)
+                    end
                     menu.addSeparator
 
                     action_saving = Qt::Action.new("Save to File", self)
-                    #  menu.add_action(action_saving)
+                    menu.add_action(action_saving)
 
                     action = menu.exec(mapToGlobal(event.pos))
                     if(action == action_scrolling)
@@ -64,7 +67,8 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "Plot2d" do
                     elsif(action == action_use_y2)
                         @options[:use_y_axis2] = !@options[:use_y_axis2]
                     elsif action == action_saving
-
+                        file_path = Qt::FileDialog::getSaveFileName(nil,"Save Plot to Pdf",File.expand_path("."),"Pdf (*.pdf)")
+                        savePdf(file_path,false,0,0) if file_path
                     end
                 end
             end
@@ -80,6 +84,15 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "Plot2d" do
                       end
         init
         @options = options.merge(@options)
+        if value.type_name == "/base/samples/SonarBeam"
+            if !@graphs.empty?
+                puts "Cannot plot SonarBeam because plot is already used!"
+                return :do_not_connect
+            else
+                @options[:multi_use_menu] = false
+                getXAxis.setLabel "Bin Number"
+            end
+        end
         graph2(value.full_name) if value.respond_to? :full_name
     end
 
@@ -106,7 +119,7 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "Plot2d" do
     end
 
     def multi_value?
-        @options[:reuse]
+        @options[:reuse] && @options[:multi_use_menu] 
     end
 
     def rename_graph(old_name,new_name)
@@ -127,7 +140,6 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "Plot2d" do
         if @options[:auto_scrolling]
             getXAxis.setRange(x-@options[:time_window],x+@options[:pre_time_window])
             graph.rescaleValueAxis(true)
-        else
         end
         replot
     end
@@ -140,8 +152,21 @@ Vizkit::UiLoader::extend_cplusplus_widget_class "Plot2d" do
         update(new_sample[2],name+"_roll")
     end
 
+    def update_sonar_beam(sample,name)
+        graph = graph2(name)
+        graph.clearData
+        sample.beam.to_a.each_with_index do |value,index|
+            graph.addData(index,value)
+        end
+        if @options[:auto_scrolling]
+            graph.rescaleKeyAxis(false)
+            graph.rescaleValueAxis(false)
+        end
+        replot
+    end
 end
 
 Vizkit::UiLoader.register_widget_for("Plot2d",Fixnum,:update)
 Vizkit::UiLoader.register_widget_for("Plot2d",Float,:update)
 Vizkit::UiLoader.register_widget_for("Plot2d",Eigen::Quaternion,:update_orientation)
+Vizkit::UiLoader.register_widget_for("Plot2d","/base/samples/SonarBeam",:update_sonar_beam)
