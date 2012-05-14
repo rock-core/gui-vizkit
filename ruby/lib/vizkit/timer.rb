82010c1c835535c5229b9e73de2c3db4d4a3f43e
Alexander Duda <Alexander@Duda.dfki.de>
2012-05-14 14:50:08 +0200

    ruby: improve Vizkit::Timer

--- ruby/lib/vizkit/timer.rb
+++ ruby/lib/vizkit/timer.rb
@@ -1,10 +1,30 @@
 module Vizkit
     class Timer
+        class << self
+            attr_reader :timer
+        end
+        @timer = Array.new
+
+        def self.stop_all
+            @timer.each do |t|
+                t.stop
+            end
+        end
+        
         def initialize(sec,single_shot=false,&block)
             @timer = Qt::Timer.new
             @timer.setSingleShot(single_shot)
             @timer.connect(SIGNAL('timeout()'),&block)
+            start(sec)
+            Timer.timer << self
+        end
+
+        def start(sec)
             @timer.start((sec*1000).to_i)
+        end
+
+        def stop
+            @timer.stop
         end
     end
 end
