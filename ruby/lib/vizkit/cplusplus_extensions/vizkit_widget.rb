be70f0c6bf65de6f1eb4c12318b2622a83d59b33
Sascha Arnold <sascha.arnold@dfki.de>
2012-06-06 13:32:07 +0200

    made TrajectoryVisualization available for type Spline<3>

--- ruby/lib/vizkit/cplusplus_extensions/vizkit_widget.rb
+++ ruby/lib/vizkit/cplusplus_extensions/vizkit_widget.rb
@@ -353,6 +353,7 @@ end
 UiLoader.register_3d_plugin('TrajectoryVisualization',"vizkit-base", 'TrajectoryVisualization')
 UiLoader.register_3d_plugin_for('TrajectoryVisualization', "/base/Vector3d", :updateTrajectory)
 UiLoader.register_3d_plugin_for('TrajectoryVisualization', "Eigen::Vector3", :updateTrajectory)
+UiLoader.register_3d_plugin_for('TrajectoryVisualization', "/base/geometry/Spline<3>", :updateSpline)
 UiLoader.register_3d_plugin('RigidBodyStateVisualization',"vizkit-base", 'RigidBodyStateVisualization')
 UiLoader.register_3d_plugin_for('RigidBodyStateVisualization', "/base/samples/RigidBodyState", :updateRigidBodyState)
 UiLoader.register_3d_plugin('LaserScanVisualization',"vizkit-base", 'LaserScanVisualization')
