rock-core/gui-vizkit

commit c98e16ccd53b1649790f8cfb2b208eba3a9e6939

tree_model: always give an async::log task context to the data model

ruby/lib/vizkit/tree_model.rb
@@ -1143,6 +1143,7 @@ module Vizkit
             @global_meta_data = GlobalMetaDataModel.new log_replay,self
             add(@global_meta_data,"-Global Meta Data-","",@global_meta_data)
             log_replay.tasks.each do |task|
+                task = Orocos::Async::Log::TaskContext.new(task)
                 model = LogTaskDataModel.new task,self
                 add(model,task.name,task.file_path,task)
             end

ruby/lib/vizkit/widgets/log_control/log_control.rb
@@ -1,4 +1,5 @@
 require "vizkit/tree_model.rb"
+require 'orocos/async/log/task_context'
 
 class LogControl
   class StopAllTimer < Qt::Object

commit e7d802c56a40bfe5699473cbdcf6aa328bf95bb6

async: fix bug introduced by c98e16ccd53b1649790f8cfb2b208eba3a9e6939

ruby/lib/vizkit/tree_model.rb
@@ -1112,20 +1112,22 @@ module Vizkit
             options[:enabled] = false
 
             add(nil,"Properties")
-            task.each_property do |prop|
-                #TODO
-                # convert to Orocos::Async::Property
-                model = TypelibDataModel.new port.read,self,:no_data => true
-                prop.on_change do |data|
-                    model.update(data)
+            task.each_property do |props|
+                props.each do |prop|
+                    model = TypelibDataModel.new prop.read,self,:no_data => true
+                    prop.on_change do |data|
+                        model.update(data)
+                    end
+                    add(model,prop.name,prop.type_name,prop)
                 end
-                add(model,prop.name,prop.type_name,prop)
             end
 
-            task.each_port do |port|
-                model = LogOutputPortDataModel.new port,self
-                add(model,port.name,port.type_name,port)
-                options[:enabled] = true if port.number_of_samples > 0
+            task.each_port do |ports|
+                ports.each do |port|
+                    model = LogOutputPortDataModel.new port,self
+                    add(model,port.name,port.type_name,port)
+                    options[:enabled] = true if port.number_of_samples > 0
+                end
             end
         end
 