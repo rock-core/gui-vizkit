566d6733106be0c6ef143be8d3330a96141b2317
Allan E. Conquest <allan.conquest@dfki.de>
2013-02-18 20:01:07 +0100

    CompoundDisplay: test script adaptions

--- ruby/test/test_compound_display.rb
+++ ruby/test/test_compound_display.rb
@@ -4,11 +4,10 @@ Orocos::CORBA.name_service.ip = '127.0.0.1'
 Orocos.initialize
 
 widget = Vizkit.default_loader.CompoundDisplay
-widget.set_grid_dimensions(4,4)
+widget.set_grid_dimensions(1,4)
 
 if ARGV[0]
     replay = Orocos::Log::Replay.open(ARGV[0])
-    widget.replay_mode(replay)
     #widget.configure_by_yaml("myconfig_avalon_obenlinks.yml")
     widget.configure(0, "front_camera", "frame", "ImageView")
     Vizkit.control replay
