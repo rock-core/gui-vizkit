d637ab7d9146e0e211449cdf7c3a619bb690d3b5
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-04-18 17:16:50 +0200

    style fix

--- ruby/lib/vizkit.rb
+++ ruby/lib/vizkit.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
 
-require 'orocos/log'
 require 'orocos/async'
 
-require File.join('vizkit','plugin_accessor.rb')
-require File.join('vizkit','typelib_qt_adapter')
-require File.join('vizkit','plugin')
-require File.join('vizkit','plugin_extensions')
-require File.join('vizkit','uiloader')
-require File.join('vizkit','vizkit')
-require File.join('vizkit','timer')
-require File.join('vizkit','connector_objects')
-require File.join('vizkit','widget_task_connector')
-require File.join('vizkit','qt_orocos')
+require 'vizkit/plugin_accessor'
+require 'vizkit/typelib_qt_adapter'
+require 'vizkit/plugin'
+require 'vizkit/plugin_extensions'
+require 'vizkit/uiloader'
+require 'vizkit/vizkit'
+require 'vizkit/timer'
+require 'vizkit/connector_objects'
+require 'vizkit/widget_task_connector'
+require 'vizkit/qt_orocos'
