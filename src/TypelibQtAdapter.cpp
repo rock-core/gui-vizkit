6595d831094bab003527f6ebeb82d6a43a3d6750
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-06-17 16:10:51 +0200

    Changed include order of header, for xenomai systems.
    xenomai uses Class signals in her headers, unfortunately qt seems
    to have a #define signals protected. Therefore signals is a reserved word.
    We now make sure that we first include the headers that include xenomai,
    and afterwords include the QT-stuff.
    
    Conflicts:
    	ext/TypelibQtAdapter.cpp

--- src/TypelibQtAdapter.cpp
+++ src/TypelibQtAdapter.cpp
@@ -1,15 +1,17 @@
-#include "TypelibQtAdapter.h"
+#include <linux/types.h>
 #include <rtt/typelib/TypelibMarshallerBase.hpp>
-#include <Qt/qmetaobject.h>
-#include <Qt/qbytearray.h>
 #include <iostream>
-#include <Qt/qcoreapplication.h>
 
 #include <rtt/typelib/TypelibMarshallerBase.hpp>
 #include <rice/Constructor.hpp>
 #include <rice/Array.hpp>
 #include <typelib_ruby.hh>
 #include <rtt/base/ChannelElementBase.hpp>
+#include <Qt/qcoreapplication.h>
+#include <Qt/qmetaobject.h>
+#include <Qt/qbytearray.h>
+#include <QtCore>
+#include "TypelibQtAdapter.h"
 
 using namespace Rice;
 
