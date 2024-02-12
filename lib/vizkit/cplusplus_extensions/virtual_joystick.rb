f40ce00a0b8ae9cac9bafac7ebc5689f35ef3212
Steffen Planthaber <steffen.planthaber@dfki.de>
2024-02-12 10:36:13 +0100

    fix occasional NaN in VirtualJoystick heading

--- lib/vizkit/cplusplus_extensions/virtual_joystick.rb
+++ lib/vizkit/cplusplus_extensions/virtual_joystick.rb
@@ -5,15 +5,17 @@ Vizkit::UiLoader.register_control_for("VirtualJoystick", "/base/commands/Motion2
 
     widget.connect(SIGNAL('axisChanged(double,double)')) do |x, y|
         value = Types.base.commands.Motion2D.new
-	value.translation = x
-	value.rotation =
-	    if x == 0 && y == 0
-		0
-	    else
-		-y.abs() * Math::atan2(y, x.abs())
-	    end
+        # the heading from Motion2D.new here is only almost 0 (2.2469905698352052e-307) due to cpp/ruby, set it to exactly 0 (as Motion2d initializes it in its constructor) to avoud occational NaN
+        value.heading.rad = 0
+        value.translation = x
+        value.rotation =
+    	    if x == 0 && y == 0
+	        	0
+	        else
+		        -y.abs() * Math::atan2(y, x.abs())
+	        end
         if block
-	    block.call(value)
+	        block.call(value)
         else
             port.write value do
             end
