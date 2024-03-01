f4af7270d7d3239f74f44a7faca6e6212bfda240
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2024-03-01 16:01:52 +0100

    Update virtual_joystick.rb

--- lib/vizkit/cplusplus_extensions/virtual_joystick.rb
+++ lib/vizkit/cplusplus_extensions/virtual_joystick.rb
@@ -5,7 +5,7 @@ Vizkit::UiLoader.register_control_for("VirtualJoystick", "/base/commands/Motion2
 
     widget.connect(SIGNAL('axisChanged(double,double)')) do |x, y|
         value = Types.base.commands.Motion2D.new
-        # the heading from Motion2D.new here is only almost 0 (2.2469905698352052e-307) due to cpp/ruby, set it to exactly 0 (as Motion2d initializes it in its constructor) to avoud occational NaN
+        # the heading from Motion2D.new here is only almost 0 (2.2469905698352052e-307) due to cpp/ruby, set it to exactly 0 (as Motion2d initializes it in its constructor) to avoid occasional NaN
         value.heading.rad = 0
         value.translation = x
         value.rotation =
